346c089b155aabf5da32e36d654ed6835d3d3f88
rufustfirefly <rufustfirefly>
2001-01-02 18:57:07 +0000

    * moved lp2pap.sh out of root of repository * small initscripts modprobe check for appletalk module in redhat * autogen now removes INSTALL/ (kludge)

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# quick fix to remove old INSTALL/ directory
+rm -Rvf INSTALL
+
+# build it all.
 libtoolize --force && aclocal $ACLOCAL_FLAGS && autoheader && automake --add-missing && autoconf
 
 # just in case automake generated errors...
