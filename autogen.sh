56c3d72de6483338b73db86d1eee69de3c1121f4
srittau <srittau>
2001-08-03 22:04:23 +0000

    Fix problems with INSTALL and README files.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# quick fix to remove old INSTALL/ directory
-rm -fr INSTALL
-
-# another fix to hack move of README
-touch README
-
 # build it all.
 libtoolize --copy && \
 	aclocal $ACLOCAL_FLAGS && \
 	autoheader && \
-	automake --include-deps --add-missing && \
+	automake --include-deps --add-missing --foreign && \
 	autoconf
 
 # just in case automake generated errors...
