b908d7ebc7c375a38e2862dc596127d58e4bd714
srittau <srittau>
2001-08-15 01:45:00 +0000

    DEC C fixes.

--- sys/generic/sys/cdefs.h
+++ sys/generic/sys/cdefs.h
@@ -1,12 +1,15 @@
 /*
- * $Id: cdefs.h,v 1.2 2001-06-29 14:14:47 rufustfirefly Exp $
+ * $Id: cdefs.h,v 1.3 2001-08-15 01:45:00 srittau Exp $
  */
 
 #ifndef _SYS_CDEFS_H
 #define _SYS_CDEFS_H 1
 
-#ifdef __STDC__
-#define __P(args)    args  
+#ifdef __STDC__ || __DECC
+/* Note that there must be exactly one space between __P(args) and args,
+ * otherwise DEC C chokes.
+ */
+#define __P(args) args
 #else /* __STDC__ */
 #define __P(args)    ()
 #endif /* __STDC__ */
