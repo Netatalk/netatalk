dba9f083515f5e2d2ff02ea737353d41f5f67ce1
srittau <srittau>
2001-10-07 11:24:41 +0000

    Sync with current Debian version.

---
+++ distrib/debian/logcheck/violations.ignore.d
@@ -0,0 +1,4 @@
+afpd\[.*\]: afp_die: asp_shutdown: Connection timed out
+afpd\[.*\]: afp_getsrvrparms: stat /.+/: Permission denied
+afpd\[.*\]: dsi_stream_read\([[:digit:]]+\): Permission denied
+afpd\[.*\]: getforkparms: (ad_refresh|of_find): Permission denied
