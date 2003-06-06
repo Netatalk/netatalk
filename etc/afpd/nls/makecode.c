f243c10c23a91a3fd89634b2e56b547eca366658
srittau <srittau>
2003-06-06 20:18:39 +0000

    New maccode map: identity to pass through characters.

--- etc/afpd/nls/makecode.c
+++ etc/afpd/nls/makecode.c
@@ -1,5 +1,5 @@
 /*
- * $Id: makecode.c,v 1.7 2002-12-01 00:44:12 srittau Exp $
+ * $Id: makecode.c,v 1.8 2003-06-06 20:18:39 srittau Exp $
  *
  * quick-and-dirty way of creating code pages
  */
@@ -15,6 +15,26 @@
 
 #include <netatalk/endian.h>
 #include "codepage.h"
+
+/* passthrough map */
+static const unsigned char identity_map[] = {
+    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+    0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
+    0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97,
+    0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
+    0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
+    0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
+    0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7,
+    0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
+    0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7,
+    0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
+    0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7,
+    0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
+    0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7,
+    0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
+    0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7,
+    0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF,
+};
 
 /* map of cp10000 (mac roman) to iso-latin-1 */
 static const unsigned char mac_to_isolatin1_map[] = {
@@ -128,13 +148,14 @@ struct mac_code {
 };
 
 static struct mac_code names[] = {
-                                     {"maccode.437", mac_to_cp437_map, sizeof(mac_to_cp437_map), "cp437"},
-                                     {"maccode.850", mac_to_cp850_map, sizeof(mac_to_cp850_map), "cp850"},
-                                     {"maccode.iso8859-1", mac_to_isolatin1_map, sizeof(mac_to_isolatin1_map), "iso8859-1"},
-                                     {"maccode.iso8859-1.adapted", mac_to_isolatin1_adapted_map, sizeof(mac_to_isolatin1_adapted_map), "iso8859-1-adapted"},
-                                     {"maccode.koi8-r",mac_to_koi8_r_map,sizeof(mac_to_koi8_r_map),"koi8-r"},
-                                     {NULL, NULL, 0, NULL}
-                                 };
+    {"maccode.identity", identity_map, sizeof(identity_map), "identity"},
+    {"maccode.437", mac_to_cp437_map, sizeof(mac_to_cp437_map), "cp437"},
+    {"maccode.850", mac_to_cp850_map, sizeof(mac_to_cp850_map), "cp850"},
+    {"maccode.iso8859-1", mac_to_isolatin1_map, sizeof(mac_to_isolatin1_map), "iso8859-1"},
+    {"maccode.iso8859-1.adapted", mac_to_isolatin1_adapted_map, sizeof(mac_to_isolatin1_adapted_map), "iso8859-1-adapted"},
+    {"maccode.koi8-r",mac_to_koi8_r_map,sizeof(mac_to_koi8_r_map),"koi8-r"},
+    {NULL, NULL, 0, NULL}
+};
 
 int main(int argc, char **argv)
 {
