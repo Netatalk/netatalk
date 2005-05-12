be874e654d396c2fdbbf6a4f22dd8bdea640fadd
bfernhomberg <bfernhomberg>
2005-05-12 00:22:26 +0000

    it's 4.3, not 4.1 *sigh*

--- bin/cnid/cnid_index.c
+++ bin/cnid/cnid_index.c
@@ -1,5 +1,5 @@
 /*
- * $Id: cnid_index.c,v 1.3 2005-05-12 00:20:24 bfernhomberg Exp $
+ * $Id: cnid_index.c,v 1.4 2005-05-12 00:22:26 bfernhomberg Exp $
  *
  * All Rights Reserved.  See COPYING.
  */
@@ -274,7 +274,7 @@ static int dbif_count(const int dbi, u_int32_t *count)
     DB_BTREE_STAT *sp;
     DB *db = db_table[dbi].db;
 
-#if DB_VERSION_MAJOR > 4 || (DB_VERSION_MAJOR == 4 && DB_VERSION_MINOR >= 1)
+#if DB_VERSION_MAJOR > 4 || (DB_VERSION_MAJOR == 4 && DB_VERSION_MINOR >= 3)
     ret = db->stat(db, db_txn, &sp, 0);
 #else
     ret = db->stat(db, &sp, 0);
