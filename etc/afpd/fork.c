7ebc13ceee6c4ddb6030fb4b504fe1531bb34ae2
Frank Lahm <franklahm@googlemail.com>
2010-09-19 09:33:50 +0200

    Better logging

--- etc/afpd/fork.c
+++ etc/afpd/fork.c
@@ -433,8 +433,8 @@ int afp_openfork(AFPObj *obj _U_, char *ibuf, size_t ibuflen _U_, char *rbuf, si
                 goto openfork_err;
                 break;
             default:
-                LOG(log_error, logtype_afpd, "afp_openfork('%s/%s'): ad_open: %s",
-                    getcwdpath, s_path->m_name, strerror(errno) );
+                LOG(log_error, logtype_afpd, "afp_openfork('%s/%s'): ad_open: errno: %i (%s)",
+                    getcwdpath, s_path->m_name, errno, strerror(errno) );
                 goto openfork_err;
                 break;
             }
