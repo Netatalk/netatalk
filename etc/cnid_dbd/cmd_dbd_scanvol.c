105df65e675eadb55d888783e731b4716142f051
Frank Lahm <franklahm@googlemail.com>
2010-09-26 11:27:32 +0200

    support interrupting -re mode

--- etc/cnid_dbd/cmd_dbd_scanvol.c
+++ etc/cnid_dbd/cmd_dbd_scanvol.c
@@ -1048,6 +1048,9 @@ static void delete_orphaned_cnids(DBD *dbd, DBD *dbd_rebuild, dbd_flags_t flags)
 
     /* Start main loop through dbd: get CNID from dbd */
     while ((dbif_idwalk(dbd, &dbd_cnid, 0)) == 1) {
+        /* Check if we got a termination signal */
+        if (alarmed)
+            longjmp(jmp, 1); /* this jumps back to cmd_dbd_scanvol() */
 
         if (deleted > 50) {
             deleted = 0;
