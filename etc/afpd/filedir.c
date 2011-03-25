017020eee1fcf3666b07e6900fcbcbd3f1281437
Frank Lahm <franklahm@googlemail.com>
2011-03-25 13:47:23 +0100

    Update did for moved stuff

--- etc/afpd/filedir.c
+++ etc/afpd/filedir.c
@@ -472,6 +472,13 @@ static int moveandrename(const struct vol *vol,
             (void)dir_remove(vol, cacheddir);
         }
 
+        /* Fixup adouble info */
+        if (!ad_metadata(oldunixname, adflags, adp)) {
+            ad_setid(adp, st->st_dev, st->st_ino, id, curdir->d_did, vol->v_stamp);
+            ad_flush(adp);
+            ad_close_metadata( adp);
+        }
+
         /* fix up the catalog entry */
         cnid_update(vol->v_cdb, id, st, curdir->d_did, upath, strlen(upath));
     }
