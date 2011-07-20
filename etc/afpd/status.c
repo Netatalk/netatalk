5469b99a80ae87f0982534e2bac4b1f43a7a81de
HAT <hat@fa2.so-net.ne.jp>
2011-07-20 22:50:15 +0900

    replace "16" with "AFPSTATUS_MACHLEN"

--- etc/afpd/status.c
+++ etc/afpd/status.c
@@ -135,10 +135,10 @@ static void status_machine(char *data)
     memcpy(&status, start + AFPSTATUS_MACHOFF, sizeof(status));
     data += ntohs( status );
 
-    if ((strlen(machine) + strlen(VERSION)) <= 16) {
+    if ((strlen(machine) + strlen(VERSION)) <= AFPSTATUS_MACHLEN) {
         len = snprintf(buf, AFPSTATUS_MACHLEN + 1, machine, VERSION);
     } else {
-        if (strlen(VERSION) > 16) {
+        if (strlen(VERSION) > AFPSTATUS_MACHLEN) {
             len = snprintf(buf, AFPSTATUS_MACHLEN + 1, VERSION);
         } else {
             (void)snprintf(buf, AFPSTATUS_MACHLEN + 1, machine, "");
