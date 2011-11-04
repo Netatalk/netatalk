309e4ab1bc757ee2b0d6f78bacd3f862cb69602a
Frank Lahm <franklahm@googlemail.com>
2011-11-04 12:48:03 +0100

    OpenBSD portability fix, header ordering, *bleah*

--- libatalk/util/socket.c
+++ libatalk/util/socket.c
@@ -27,15 +27,15 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <sys/uio.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/time.h>
 #include <time.h>
 #include <sys/ioctl.h>
-#include <sys/uio.h>
 
 #include <atalk/logger.h>
 #include <atalk/util.h>
