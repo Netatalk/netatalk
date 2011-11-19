Netatalk/netatalk

commit f422a450f02e1bc95a26a6b10f1446cc291d5007

Performance tuning in afp_read: factor out an unneccessary filesytem read when sendfile is available

etc/afpd/fork.c
@@ -844,7 +844,9 @@ static int read_fork(AFPObj *obj, char *ibuf, size_t ibuflen _U_, char *rbuf, si
     int			eid, xlate = 0;
     u_int16_t		ofrefnum;
     u_char		nlmask, nlchar;
-    
+
+    *rbuflen = 0;
+
     ibuf += 2;
     memcpy(&ofrefnum, ibuf, sizeof( ofrefnum ));
     ibuf += sizeof( u_short );
@@ -900,12 +902,12 @@ static int read_fork(AFPObj *obj, char *ibuf, size_t ibuflen _U_, char *rbuf, si
         goto afp_read_err;
     }
 
-#define min(a,b)	((a)<(b)?(a):(b))
-    *rbuflen = min( reqcount, *rbuflen );
+#ifndef WITH_SENDFILE
+    *rbuflen = MIN(reqcount, *rbuflen);
     err = read_file(ofork, eid, offset, nlmask, nlchar, rbuf, rbuflen, xlate);
     if (err < 0)
         goto afp_read_done;
-
+#endif
     /* dsi can stream requests. we can only do this if we're not checking
      * for an end-of-line character. oh well. */
     if ((obj->proto == AFPPROTO_DSI) && (*rbuflen < reqcount) && !nlmask) {

include/atalk/util.h
@@ -47,6 +47,12 @@
 #endif /* NDEBUG */
 
 #define STRCMP(a,b,c) (strcmp(a,c) b 0)
+#ifndef MAX
+#define MAX(a,b) ((a) > (b) ? a : b)
+#endif
+#ifndef MIN
+#define MIN(a,b) ((a) < (b) ? a : b)
+#endif
 
 #if BYTE_ORDER == BIG_ENDIAN
 #define hton64(x)       (x)

libatalk/dsi/dsi_read.c
@@ -22,10 +22,7 @@
 #endif
 
 #include <atalk/dsi.h>
-
-#ifndef min
-#define min(a,b)   ((a) < (b) ? (a) : (b))
-#endif /* ! min */
+#include <atalk/util.h>
 
 /* streaming i/o for afp_read. this is all from the perspective of the
  * client. it basically does the reverse of dsi_write. on first entry,
@@ -45,7 +42,7 @@ ssize_t dsi_readinit(DSI *dsi, void *buf, const size_t buflen,
   dsi->in_write++;
   if (dsi_stream_send(dsi, buf, buflen)) {
     dsi->datasize = size - buflen;
-    return min(dsi->datasize, buflen);
+    return MIN(dsi->datasize, buflen);
   }
 
   return -1; /* error */
@@ -65,7 +62,7 @@ ssize_t dsi_read(DSI *dsi, void *buf, const size_t buflen)
 
   if (len == buflen) {
     dsi->datasize -= len;
-    return min(dsi->datasize, buflen);
+    return MIN(dsi->datasize, buflen);
   }
 
   return -1;

commit bce50773d2a976ee9a81a5114476628e573bda94

Fix regression introduced by f422a450f02e1bc95a26a6b10f1446cc291d5007

etc/afpd/fork.c
@@ -845,8 +845,6 @@ static int read_fork(AFPObj *obj, char *ibuf, size_t ibuflen _U_, char *rbuf, si
     u_int16_t		ofrefnum;
     u_char		nlmask, nlchar;
 
-    *rbuflen = 0;
-
     ibuf += 2;
     memcpy(&ofrefnum, ibuf, sizeof( ofrefnum ));
     ibuf += sizeof( u_short );
@@ -902,8 +900,8 @@ static int read_fork(AFPObj *obj, char *ibuf, size_t ibuflen _U_, char *rbuf, si
         goto afp_read_err;
     }
 
-#ifndef WITH_SENDFILE
     *rbuflen = MIN(reqcount, *rbuflen);
+#ifndef WITH_SENDFILE
     err = read_file(ofork, eid, offset, nlmask, nlchar, rbuf, rbuflen, xlate);
     if (err < 0)
         goto afp_read_done;