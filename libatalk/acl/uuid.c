ae725246497a7bd8b8f69977a886386368e40e6c
Frank Lahm <franklahm@googlemail.com>
2012-03-09 11:20:40 +0100

    Fix dependencie on LDAP

--- libatalk/acl/uuid.c
+++ libatalk/acl/uuid.c
@@ -112,9 +112,11 @@ const char *uuid_bin2string(unsigned char *uuid) {
     int i = 0;
     unsigned char c;
 
+#ifdef HAVE_LDAP
     if (ldap_uuid_string)
         uuidmask = ldap_uuid_string;
     else
+#endif
         uuidmask = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
 
     LOG(log_debug, logtype_afpd, "uuid_bin2string{uuid}: mask: %s", uuidmask);
