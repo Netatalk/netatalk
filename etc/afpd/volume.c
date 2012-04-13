4a151aa953ceb6ec985bd332a847d445cb37c567
Frank Lahm <franklahm@googlemail.com>
2012-04-13 10:54:08 +0200

    Fix volume message

--- etc/afpd/volume.c
+++ etc/afpd/volume.c
@@ -873,11 +873,8 @@ int afp_openvol(AFPObj *obj, char *ibuf, size_t ibuflen _U_, char *rbuf, size_t 
         }
 
         char *msg;
-        if ((msg = iniparser_getstrdup(obj->iniconfig, volume->v_configname, "login message",  NULL)) != NULL) {
-            if (obj->options.loginmesg)
-                free(obj->options.loginmesg);
-            obj->options.loginmesg = msg;
-        }
+        if ((msg = iniparser_getstring(obj->iniconfig, volume->v_configname, "login message",  NULL)) != NULL)
+            setmessage(msg);
 
         return( AFP_OK );
     }
