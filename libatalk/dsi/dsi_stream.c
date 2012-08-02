Netatalk/netatalk

commit 88436a544d0c87299a3417432b15e897f1ea15ec

Convert to more readable error checking

libatalk/dsi/dsi_stream.c
@@ -400,7 +400,7 @@ ssize_t dsi_stream_read_file(DSI *dsi, const int fromfd, off_t offset, const siz
 #endif /* defined(SOLARIS) || defined(FREEBSD) */
 #endif /* HAVE_SENDFILEV */
 
-                if (dsi_peek(dsi)) {
+                if (dsi_peek(dsi) != 0) {
                     ret = -1;
                     goto exit;
                 }
@@ -533,7 +533,7 @@ int dsi_stream_send(DSI *dsi, void *buf, size_t length)
           break;
       else if (len < 0) { /* error */
           if (errno == EAGAIN || errno == EWOULDBLOCK) {
-              if (!dsi_peek(dsi)) {
+              if (dsi_peek(dsi) != 0) {
                   continue;
               }
           }

commit 715cb9c1eb1398d49e07d5108c349bbe0307f4cc

Fix regression introduced by 88436a544d0c87299a3417432b15e897f1ea15ec

libatalk/dsi/dsi_stream.c
@@ -533,7 +533,7 @@ int dsi_stream_send(DSI *dsi, void *buf, size_t length)
           break;
       else if (len < 0) { /* error */
           if (errno == EAGAIN || errno == EWOULDBLOCK) {
-              if (dsi_peek(dsi) != 0) {
+              if (dsi_peek(dsi) == 0) {
                   continue;
               }
           }