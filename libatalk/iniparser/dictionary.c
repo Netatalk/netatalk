7e64456933e84c6be8371c7256e4d617ff004f99
Ralph Boehme <sloowfranklin@gmail.com>
2013-01-02 18:46:45 +0100

    Include config.h

--- libatalk/iniparser/dictionary.c
+++ libatalk/iniparser/dictionary.c
@@ -19,6 +19,10 @@
 /*---------------------------------------------------------------------------
    								Includes
  ---------------------------------------------------------------------------*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <atalk/dictionary.h>
 #include <atalk/compat.h>
 
