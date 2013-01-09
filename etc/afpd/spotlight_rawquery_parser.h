a151430b3b4e8d84f91a3a61545ce4bcc00dc4ca
Ralph Boehme <sloowfranklin@gmail.com>
2013-01-09 10:33:16 +0100

    Remove spotlight_rawquery_lexer.h

--- etc/afpd/spotlight_rawquery_parser.h
+++ etc/afpd/spotlight_rawquery_parser.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 2.5.  */
+/* A Bison parser, made by GNU Bison 2.7.  */
 
 /* Bison interface for Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -30,6 +30,15 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -71,12 +80,10 @@
 
 
 
-
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
 {
-
-/* Line 2068 of yacc.c  */
+/* Line 2058 of yacc.c  */
 #line 45 "spotlight_rawquery_parser.y"
 
     int ival;
@@ -85,9 +92,8 @@ typedef union YYSTYPE
     time_t tval;
 
 
-
-/* Line 2068 of yacc.c  */
-#line 91 "spotlight_rawquery_parser.h"
+/* Line 2058 of yacc.c  */
+#line 97 "spotlight_rawquery_parser.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
@@ -96,10 +102,21 @@ typedef union YYSTYPE
 
 extern YYSTYPE yylval;
 
-
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
 /* "%code provides" blocks.  */
-
-/* Line 2068 of yacc.c  */
+/* Line 2058 of yacc.c  */
 #line 39 "spotlight_rawquery_parser.y"
 
   #define SPRAW_TIME_OFFSET 978307200
@@ -107,6 +124,7 @@ extern YYSTYPE yylval;
   extern slq_t *ssp_slq;
 
 
+/* Line 2058 of yacc.c  */
+#line 129 "spotlight_rawquery_parser.h"
 
-/* Line 2068 of yacc.c  */
-#line 113 "spotlight_rawquery_parser.h"
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
