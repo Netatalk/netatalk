01e4f40259950a534d8608e0b3e68e50fb1ca233
Ralph Boehme <sloowfranklin@gmail.com>
2013-02-19 17:35:04 +0100

    Several fixes in ad_conv_dehex()
    Testing whether we need to perform dehexing was done by a simple
    strchr(':'), thus names only containing a single ':' would trigger
    dehexing even though they might not contain the relevant strings
    ":2e" or :2f".
    
    Also fix a ressource leak in bfindreplace() and simplify the
    check whether a rename took place in enumerate.c.

--- etc/afpd/enumerate.c
+++ etc/afpd/enumerate.c
@@ -362,16 +362,14 @@ static int enumerate(AFPObj *obj _U_, char *ibuf, size_t ibuflen _U_,
 
         /* conversions on the fly */
         const char *convname;
-        if (ad_convert(sd.sd_last, &s_path.st, vol, &convname) == 0 && convname) {
-            s_path.u_name = (char *)convname;
-        }
-
-        /* Fixup CNID db if ad_convert resulted in a rename (then convname != NULL) */
-        if (convname) {
-            s_path.id = cnid_lookup(vol->v_cdb, &s_path.st, curdir->d_did, sd.sd_last, strlen(sd.sd_last));
-            if (s_path.id != CNID_INVALID) {
-                if (cnid_update(vol->v_cdb, s_path.id, &s_path.st, curdir->d_did, (char *)convname, strlen(convname)) != 0)
-                    LOG(log_error, logtype_afpd, "enumerate: error updating CNID of \"%s\"", fullpathname(convname));
+        if (ad_convert(sd.sd_last, &s_path.st, vol, &convname) == 0) {
+            if (convname) {
+                s_path.u_name = (char *)convname;
+                s_path.id = cnid_lookup(vol->v_cdb, &s_path.st, curdir->d_did, sd.sd_last, strlen(sd.sd_last));
+                if (s_path.id != CNID_INVALID) {
+                    if (cnid_update(vol->v_cdb, s_path.id, &s_path.st, curdir->d_did, (char *)convname, strlen(convname)) != 0)
+                        LOG(log_error, logtype_afpd, "enumerate: error updating CNID of \"%s\"", fullpathname(convname));
+                }
             }
         }
 
