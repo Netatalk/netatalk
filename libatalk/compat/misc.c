1154c8838177cf4dfd9c85aa47df4ba70335e9cc
Ralph Boehme <sloowfranklin@gmail.com>
2013-10-29 11:05:59 +0100

    Add asprint() compatibility function for systems lacking it

--- libatalk/compat/misc.c
+++ libatalk/compat/misc.c
@@ -61,3 +61,17 @@ int vasprintf(char **ret, const char *fmt, va_list ap)
     }
 }
 #endif
+
+#ifndef HAVE_ASPRINTF
+int asprintf(char **strp, const char *fmt, ...)
+{
+    va_list ap;
+    int len;
+
+    va_start(ap, fmt);
+    len = vasprintf(strp, fmt, ap);
+    va_end(ap);
+
+    return len;
+}
+#endif
