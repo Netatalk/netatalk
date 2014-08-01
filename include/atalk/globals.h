d7e5521f69859b3df89da09bf50621945f119d97
Ralph Boehme <rb@sernet.de>
2014-08-01 16:09:03 +0200

    afpd: check for modified  included config file, FR #95
    Signed-off-by: Ralph Boehme <rb@sernet.de>

--- include/atalk/globals.h
+++ include/atalk/globals.h
@@ -131,6 +131,7 @@ struct afp_options {
     char *cnid_mysql_pw;
     char *cnid_mysql_db;
     struct afp_volume_name volfile;
+    struct afp_volume_name includefile;
     uint64_t sparql_limit;
 };
 
