ba23d139f8c6d286cb09a75fdc6d73cd412f4664
Hauke Fath <hauke@Anscharte.nt.e-technik.tu-darmstadt.de>
2017-04-13 16:10:30 +0200

    Correct variable name tyop in assert().

--- etc/afpd/afp_mdns.c
+++ etc/afpd/afp_mdns.c
@@ -167,7 +167,7 @@ static void register_stuff(const AFPConfig *configs) {
 
     // Allocate the memory to store our service refs
     svc_refs = calloc(svc_ref_count, sizeof(DNSServiceRef));
-    assert(svc_ref);
+    assert(svc_refs);
     svc_ref_count = 0;
 
     /* AFP server */
