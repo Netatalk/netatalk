399c6f638286455988b0e9d00f2ae4324e6fee7b
HAT <hat@fa2.so-net.ne.jp>
2017-05-24 00:34:28 +0900

    build with LibreSSL
    could not build with LibreSSL when supporting OpenSSL 1.1.
    
    See also
    b4a80257faee4bba0c2f54d4e78d7f3aa54f69c2
    
    Bug: <https://github.com/Netatalk/Netatalk/issues/91>
    
    Signed-off-by: elij <elij@wundrd.net>
    Reviewed-by: HAT <hat@fa2.so-net.ne.jp>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- etc/uams/openssl_compat.h
+++ etc/uams/openssl_compat.h
@@ -11,7 +11,7 @@ http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #ifndef OPENSSL_COMPAT_H
 #define OPENSSL_COMPAT_H
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 inline static int DH_set0_pqg(DH *dh, BIGNUM *p, BIGNUM *q, BIGNUM *g)
 {
    /* If the fields p and g in d are NULL, the corresponding input
