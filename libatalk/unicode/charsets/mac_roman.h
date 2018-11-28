1349549d5274daada7530103f90a9a7fc3952985
Andrew Bauer <zonexpertconsulting@outlook.com>
2018-11-28 08:52:00 -0600

    update gpl mailing address Reviewed-by: Ralph Boehme <slow@samba.org>

--- libatalk/unicode/charsets/mac_roman.h
+++ libatalk/unicode/charsets/mac_roman.h
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * from GNU libiconv
  * modified by Bjorn (Apple Logo 0xf0 to Apple unicode 0xf8ff )
