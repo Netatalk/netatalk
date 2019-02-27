f47f768a4dd926090dc1bea42c090face8d95c89
Simon Muras <s.muras@laudert.de>
2019-02-27 13:28:55 +0100

    NETATALK: Added Ignore Directories Feature

--- etc/afpd/fce_api.c
+++ etc/afpd/fce_api.c
@@ -88,6 +88,7 @@ static uint8_t fce_ev_info;    /* flags of additional info to send in events */
 #define MAXIOBUF 4096
 static unsigned char iobuf[MAXIOBUF];
 static const char **skip_files;
+static const char **skip_directories;
 static struct fce_close_event last_close_event;
 
 static char *fce_event_names[] = {
@@ -495,6 +496,33 @@ static void fce_init_ign_names(const char *ignores)
     free(names);
 }
 
+static void fce_init_ign_directories(const char *ignores)
+{
+    int count = 0;
+    char *names = strdup(ignores);
+    char *p;
+    int i = 0;
+
+    while (names[i]) {
+        count++;
+        for (; names[i] && names[i] != ','; i++)
+            ;
+        if (!names[i])
+            break;
+        i++;
+    }
+
+    skip_directories = calloc(count + 1, sizeof(char *));
+
+    for (i = 0, p = strtok(names, ","); p ; p = strtok(NULL, ",")) {
+        char *tmp = strdup("/");
+        skip_directories[i++] = strdup(strcat(strcat(tmp,p), "/"));
+     }
+
+    free(names);
+}
+
+
 /*
  *
  * Dispatcher for all incoming file change events
@@ -504,6 +532,7 @@ int fce_register(const AFPObj *obj, fce_ev_t event, const char *path, const char
 {
     static bool first_event = true;
     const char *bname;
+    const char *dirname;
 
     if (!(fce_ev_enabled & (1 << event)))
         return AFP_OK;
@@ -515,6 +544,7 @@ int fce_register(const AFPObj *obj, fce_ev_t event, const char *path, const char
         path, fce_event_names[event]);
 
     bname = basename_safe(path);
+    dirname = realpath_safe(path);
 
     if ((udp_sockets == 0) && (obj->fce_notify_script == NULL)) {
         /* No listeners configured */
@@ -525,6 +555,9 @@ int fce_register(const AFPObj *obj, fce_ev_t event, const char *path, const char
 	if (first_event) {
 		fce_initialize_history();
         fce_init_ign_names(obj->fce_ign_names);
+        if (obj->fce_ign_directories != NULL){
+            fce_init_ign_directories(obj->fce_ign_directories);
+        }
         first_event = false;
 	}
 
@@ -533,6 +566,12 @@ int fce_register(const AFPObj *obj, fce_ev_t event, const char *path, const char
         if (strcmp(bname, skip_files[i]) == 0)
 			return AFP_OK;
 	}
+	if (skip_directories != NULL){
+	   for (int i = 0; skip_directories[i] != NULL; i++) {
+		    if (strstr(dirname, skip_directories[i]))
+		 	   return AFP_OK;
+       }
+    }
 
 	/* Can we ignore this event based on type or history? */
 	if (fce_handle_coalescation(event, path)) {
@@ -602,7 +641,7 @@ int fce_set_events(const char *events)
 {
     char *e;
     char *p;
-    
+
     if (events == NULL)
         return AFPERR_PARAM;
 
