e73599f7de82a6396a8f61078c4e1cc1285dc59c
Jose Quinteiro <github@quinteiro.org>
2020-03-31 12:52:00 -0700

    Fix sign extension problem in bsd_attr_list()
    In FreeBSD xattr names can be up to 255 bytes long. Reading the length
    byte in an attr list as a signed char will result in negative values.
    Noticed by Peter Rutenbar in Sourcefor bug #659.
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- libatalk/vfs/extattr.c
+++ libatalk/vfs/extattr.c
@@ -353,13 +353,13 @@ static ssize_t bsd_attr_list (int type, extattr_arg arg, char *list, size_t size
     }
 
     /* Convert from pascal strings to C strings */
-    len = list[0];
+    len = (unsigned char)list[0];
     memmove(list, list + 1, list_size - 1);
 
     for(i = len; i < list_size; ) {
         LOG(log_maxdebug, logtype_afpd, "len: %d, i: %d", len, i);
 
-        len = list[i];
+        len = (unsigned char)list[i];
         list[i] = '\0';
         i += len + 1;
     }
