54ed76d390f0e2e848bc6bf6d357a3128a6d6820
Daniel Markstedt <markstedt@gmail.com>
2021-12-08 11:49:51 -0800

    Cleanup layout

--- etc/atalkd/rtmp.h
+++ etc/atalkd/rtmp.h
@@ -69,14 +69,14 @@ struct rtmp_tuple {
 #ifndef BSD4_4
 #define RTMP_ADD	SIOCADDRT
 #define RTMP_DEL	SIOCDELRT
-#define OS_STARTUP_FIRSTNET    0
+#define OS_STARTUP_FIRSTNET 	0
 #else /* BSD4_4 */
 #define RTMP_ADD	RTM_ADD
 #define RTMP_DEL	RTM_DELETE
-#define OS_STARTUP_FIRSTNET    1
+#define OS_STARTUP_FIRSTNET 	1
 #endif /* BSD4_4 */
 
-#define STARTUP_FIRSTNET    0xff00
+#define STARTUP_FIRSTNET 	0xff00
 #define STARTUP_LASTNET		0xfffe
 
 extern int	rtfd;
