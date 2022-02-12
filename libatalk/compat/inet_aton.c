4ded89da05a2a9299297da20afaa8f067e7a873f
Daniel Markstedt <markstedt@gmail.com>
2022-02-11 16:38:37 -0800

    Remove obsolete platform code, while adding back Solaris platform code.

--- libatalk/compat/inet_aton.c
+++ libatalk/compat/inet_aton.c
@@ -2,11 +2,22 @@
  * $Id: inet_aton.c,v 1.4 2003-02-17 01:51:08 srittau Exp $
  */
 
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif /* HAVE_CONFIG_H */
-
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#ifdef sun
+#if defined(sun) && defined(__svr4__)
+#ifndef INADDR_NONE
+#define INADDR_NONE ((unsigned) 0xffffffff)
+#endif /* ! INADDR_NONE */
+
+int inet_aton(const char *name, struct in_addr *addr)
+{
+  if ((addr->s_addr = inet_addr(name)) == htonl(INADDR_NONE))
+    return 0;
+
+  return 1;
+}
+#endif /* sun && __svr4__ */
--- libatalk/compat/misc.c
+++ libatalk/compat/misc.c
@@ -1,4 +1,9 @@
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif /* HAVE_CONFIG_H */
 
+#if !defined HAVE_DIRFD && defined SOLARIS
+#include <dirent.h>
+int dirfd(DIR *dir)
+{
+    return dir->d_fd;
+}
+#endif
