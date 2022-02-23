ced8b9fa7b9b1f7c08b70a006819ecce9c083c3a
Daniel Markstedt <markstedt@gmail.com>
2022-02-23 08:44:04 -0800

    Clean up trailing spaces.

--- etc/uams/uams_dhx_pam.c
+++ etc/uams/uams_dhx_pam.c
@@ -2,7 +2,7 @@
  * $Id: uams_dhx_pam.c,v 1.33 2010-03-30 10:25:49 franklahm Exp $
  *
  * Copyright (c) 1990,1993 Regents of The University of Michigan.
- * Copyright (c) 1999 Adrian Sun (asun@u.washington.edu) 
+ * Copyright (c) 1999 Adrian Sun (asun@u.washington.edu)
  * All Rights Reserved.  See COPYRIGHT.
  */
 
--- etc/uams/uams_randnum.c
+++ etc/uams/uams_randnum.c
@@ -1,8 +1,8 @@
-/* 
+/*
  * $Id: uams_randnum.c,v 1.21 2010-03-30 10:25:49 franklahm Exp $
  *
  * Copyright (c) 1990,1993 Regents of The University of Michigan.
- * Copyright (c) 1999 Adrian Sun (asun@u.washington.edu) 
+ * Copyright (c) 1999 Adrian Sun (asun@u.washington.edu)
  * All Rights Reserved.  See COPYRIGHT.
  */
 
