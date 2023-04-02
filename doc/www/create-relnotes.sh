5431403c326a449088afa3e6a053430b421dff3c
Daniel Markstedt <markstedt@gmail.com>
2023-04-02 12:27:00 -0700

    Remove release notes code since it's no longer used

--- doc/www/create-relnotes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# 
-# This script requires asciidoc http://www.methods.co.nz/asciidoc/
-
-# (echo -e "Netatalk NEWS\n-------------\n\n" ; cat NEWS) | ./asciidoc.py -f netatalk-news.conf -b css -o NEWS.html - && chmod g+w NEWS.html
-./asciidoc.py -f netatalk-relnotes.conf -b html5 -o ReleaseNotes.html ReleaseNotes && chmod g+w ReleaseNotes.html
