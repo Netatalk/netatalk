4a3142414fab70ff0f53b4be185dcc8846a18fc5
dgsga <181612+dgsga@users.noreply.github.com>
2023-05-21 15:26:57 +0100

    Use correct FreeBSD/NetBSD define in ad_cp.c

--- bin/ad/ad_cp.c
+++ bin/ad/ad_cp.c
@@ -841,7 +841,7 @@ static int setfile(const struct stat *fs, int fd)
     islink = !fdval && S_ISLNK(fs->st_mode);
     mode = fs->st_mode & (S_ISUID | S_ISGID | S_ISVTX | S_IRWXU | S_IRWXG | S_IRWXO);
 
-#if defined(__FreeBSD__) || defined(__NetBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__APPLE__)
     TIMESPEC_TO_TIMEVAL(&tv[0], &fs->st_atimespec);
     TIMESPEC_TO_TIMEVAL(&tv[1], &fs->st_mtimespec);
 #else
