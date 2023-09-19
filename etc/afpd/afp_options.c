5e05f3b912619eed2730b4b6bc06e3ff88ace432
dgsga <181612+dgsga@users.noreply.github.com>
2023-09-19 08:19:05 +0100

    Remove quota support from Netatalk 3

--- etc/afpd/afp_options.c
+++ etc/afpd/afp_options.c
@@ -122,13 +122,6 @@ static void show_version_extended(void )
 	puts( "No" );
 #endif
 
-	printf( "         Quota support:\t" );
-#ifndef NO_QUOTA_SUPPORT
-	puts( "Yes" );
-#else
-	puts( "No" );
-#endif
-
 	printf( "   Admin group support:\t" );
 	puts( "Yes" );
 
