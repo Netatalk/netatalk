7888c9afb230afed89eacab4bba838cac5bebb18
Daniel Markstedt <markstedt@gmail.com>
2023-10-09 08:55:30 +0900

    Remove unused variables

--- libatalk/acl/cache.c
+++ libatalk/acl/cache.c
@@ -46,7 +46,6 @@ cacheduser_t *uuidcache[256];   /* indexed by hash of uuid */
 
 void uuidcache_dump(void) {
     int i;
-    int ret = 0;
     cacheduser_t *entry;
     char timestr[200];
     struct tm *tmp = NULL;
@@ -301,7 +300,6 @@ int add_cachebyuuid( uuidp_t inuuid, const char *inname, uuidtype_t type, const 
     char *name = NULL;
     uuidp_t uuid = NULL;
     cacheduser_t *cacheduser = NULL;
-    cacheduser_t *entry;
     unsigned char hash;
 
     /* allocate mem and copy values */
