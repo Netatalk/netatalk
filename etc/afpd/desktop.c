goshhhy/netatalk-classic

Add casts to permit compilation on 32-bit platforms.

etc/afpd/desktop.c
@@ -228,7 +228,7 @@ int afp_addicon(AFPObj * obj, char *ibuf, size_t ibuflen _U_, char *rbuf,
 
 		if (debug)
 			if (obj->options.flags & OPTION_DEBUG) {
-				printf("(write) len: %ld\n", buflen);
+				printf("(write) len: %ld\n", (unsigned long) buflen);
 				bprint(rbuf, buflen);
 			}
 

Netatalk/netatalk

etc/afpd/desktop.c
@@ -227,7 +227,7 @@ addicon_err:
 
 #ifdef DEBUG1
         if (obj->options.flags & OPTION_DEBUG) {
-            printf("(write) len: %d\n", buflen);
+            printf("(write) len: %d\n", (unsigned long) buflen);
             bprint(rbuf, buflen);
         }
 #endif