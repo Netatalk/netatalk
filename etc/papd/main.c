35ee222e88474599030b1d5219aa5074ece5c5b2
Daniel Markstedt <markstedt@gmail.com>
2023-10-14 22:35:01 +0900

    Use string directly instead of with macro; define snprintf format

--- etc/papd/main.c
+++ etc/papd/main.c
@@ -57,8 +57,6 @@
 #include "print_cups.h"
 
 
-#define PIPED_STATUS	"status: print spooler processing job"
-
 /* This maps to TReq and TResp, per "Inside AppleTalk" 10-13 */
 
 typedef struct __attribute__((__packed__)) {
@@ -77,7 +75,7 @@ int		debug = 0;
 static char	*conffile = _PATH_PAPDCONF;
 char		*printcap = _PATH_PAPDPRINTCAP;
 unsigned char	connid, quantum, sock, oquantum = PAP_MAXQUANTUM;
-char		*cannedstatus = PIPED_STATUS;
+char		*cannedstatus = "status: print spooler processing job";
 struct printer	*printer = NULL;
 char		*version = VERSION;
 static char	*pidfile = _PATH_PAPDLOCK;
@@ -564,12 +562,12 @@ int getstatus(struct printer *pr, rbuf_t *buf)
 #ifdef HAVE_CUPS
     if ( pr->p_flags & P_PIPED ) {
 	buf->buf_len = strlen(cannedstatus);
-	snprintf(buf->buf, 254, cannedstatus);
+	snprintf(buf->buf, 254, "%s", cannedstatus);
 	return (buf->buf_len + 1);
     } else {
 	cups_get_printer_status( pr );
 	buf->buf_len = strlen(pr->p_status);
-	snprintf(buf->buf, 254, pr->p_status);
+	snprintf(buf->buf, 254, "%s", pr->p_status);
 	return (buf->buf_len + 1);
     }
 #else
@@ -586,7 +584,7 @@ int getstatus(struct printer *pr, rbuf_t *buf)
 
     if ((pr->p_flags & P_PIPED) || (fd == NULL)) {
 	buf->buf_len = strlen(cannedstatus);
-	snprintf(buf->buf, 254, cannedstatus);
+	snprintf(buf->buf, 254, "%s", cannedstatus);
 	return (buf->buf_len + 1);
     } else {
 	rc = fread(getstatus_buffer, 255, sizeof(unsigned char), fd);
@@ -595,11 +593,11 @@ int getstatus(struct printer *pr, rbuf_t *buf)
 	if (rc > 0) {
 		if (temp != NULL) {
 			temp[strcspn(temp, "\n")] = '\0';
-			snprintf(buf->buf, 254, temp);
+			snprintf(buf->buf, 254, "%s", temp);
 			buf->buf_len = strlen(buf->buf);
 		}
 	} else {
-		snprintf(buf->buf, 254, "thisisanemptystring");
+		snprintf(buf->buf, 254, "%s", "thisisanemptystring");
 		buf->buf_len = 0;
 	}
 
