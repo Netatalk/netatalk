1bc5dcf5ce23593e62f65b08cb9711df05e26d5e
Daniel Markstedt <daniel@mindani.net>
2023-11-27 03:44:28 +0900

    pam_conv: Only Solarish seems to use non-const struct type. Issue #560

--- etc/uams/uams_dhx2_pam.c
+++ etc/uams/uams_dhx2_pam.c
@@ -136,7 +136,7 @@ error:
  * echo off means password.
  */
 static int PAM_conv (int num_msg,
-#ifdef LINUX
+#if !defined(__svr4__)
                      const struct pam_message **msg,
 #else
                      struct pam_message **msg,
