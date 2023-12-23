78e1dece5123ebd53f6e02d1e701e8f77e9aa6cc
Daniel Markstedt <daniel@mindani.net>
2023-12-23 22:54:33 +0900

    Improve logger_test, while re-enabling the syslog tests, Issue #584

--- bin/misc/logger_test.c
+++ bin/misc/logger_test.c
@@ -6,41 +6,49 @@
 int main(int argc, char *argv[])
 {
   set_processname("logger_Test");
-#if 0
-  LOG(log_severe, logtype_logger, "Logging Test starting: this should only log to syslog");
+
+  setuplog("Default LOG_SEVERE");
+  LOG(log_severe, logtype_logger, "[1/4] Logging Test starting: this should only log to syslog");
 
   /* syslog testing */
+
   LOG(log_severe, logtype_logger, "Disabling syslog logging.");
   unsetuplog("Default");
   LOG(log_error, logtype_default, "This shouldn't log to syslog: LOG(log_error, logtype_default).");
   LOG(log_error, logtype_logger, "This shouldn't log to syslog: LOG(log_error, logtype_logger).");
   setuplog("Default LOG_INFO");
-  LOG(log_info, logtype_logger, "Set syslog logging to 'log_info', so this should log again. LOG(log_info, logtype_logger).");
-  LOG(log_error, logtype_logger, "This should log to syslog: LOG(log_error, logtype_logger).");
-  LOG(log_error, logtype_default, "This should log to syslog. LOG(log_error, logtype_default).");
+  LOG(log_info, logtype_logger, "[2/4] Set syslog logging to 'log_info', so this should log again. LOG(log_info, logtype_logger).");
+  LOG(log_error, logtype_logger, "[3/4] This should log to syslog: LOG(log_error, logtype_logger).");
+  LOG(log_error, logtype_default, "[4/4] This should log to syslog. LOG(log_error, logtype_default).");
   LOG(log_debug, logtype_logger, "This shouldn't log to syslog. LOG(log_debug, logtype_logger).");
   LOG(log_debug, logtype_default, "This shouldn't log to syslog. LOG(log_debug, logtype_default).");
   LOG(log_severe, logtype_logger, "Disabling syslog logging.");
   unsetuplog("Default");
-#endif
+
   /* filelog testing */
 
+  setuplog("Default LOG_SEVERE test.log");
+  LOG(log_severe, logtype_logger, "[1/4] Logging Test starting: this should only log to file");
+
   setuplog("DSI log_maxdebug test.log");
-  LOG(log_info, logtype_dsi, "This should log.");
+  LOG(log_info, logtype_dsi, "[2/4] This should log.");
   LOG(log_error, logtype_default, "This should not log.");
 
   setuplog("Default log_debug test.log");
-  LOG(log_debug, logtype_default, "This should log.");
+  LOG(log_debug, logtype_default, "[3/4] This should log.");
   LOG(log_maxdebug, logtype_default, "This should not log.");
 
-  LOG(log_maxdebug, logtype_dsi, "This should still log.");
+  LOG(log_maxdebug, logtype_dsi, "[4/4] This should still log.");
 
   /* flooding prevention check */
+
   LOG(log_debug, logtype_default, "Flooding 3x");
   for (int i = 0; i < 3; i++) {
       LOG(log_debug, logtype_default, "Flooding...");
   }
+
   /* wipe the array */
+
   LOG(log_debug, logtype_default, "1"); LOG(log_debug, logtype_default, "2"); LOG(log_debug, logtype_default, "3");
 
   LOG(log_debug, logtype_default, "-============");
@@ -90,5 +98,4 @@ int main(int argc, char *argv[])
 
   return 0;
 }
-
 
