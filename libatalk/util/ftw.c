008fedaa582afe76a1e311e0181f180593810875
Daniel Markstedt <daniel@mindani.net>
2024-01-06 02:37:39 +0900

    [2.3] Two small memory handling patches ported from netatalk-classic (#645)
    * Use fseek() here too.
    
    * Properly handle possible use-after-free.
    
    ---------
    
    Co-authored-by: Christopher Kobayashi <software+github@disavowed.jp>
    Co-authored-by: Chris Kobayashi <10285168+christopherkobayashi@users.noreply.github.com>

--- libatalk/util/ftw.c
+++ libatalk/util/ftw.c
@@ -318,8 +318,8 @@ open_dir_stream (int *dfdp, struct ftw_data *data, struct dir_data *dirp)
             buf[actsize++] = '\0';
 
             /* Shrink the buffer to what we actually need.  */
-            data->dirstreams[data->actdir]->content = realloc (buf, actsize);
-            if (data->dirstreams[data->actdir]->content == NULL)
+            if ( (data->dirstreams[data->actdir]->content =
+                realloc(buf, actsize)) == NULL)
             {
                 int save_err = errno;
                 free (buf);
