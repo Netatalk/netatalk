541c8686ce9b9720858f492410186001bee27954
dgsga <181612+dgsga@users.noreply.github.com>
2024-04-12 16:53:35 +0100

    [2.4] Remove obsolete Solaris Appletalk kernel module code

--- sys/solaris/ioc.h
+++
@@ -1,4 +0,0 @@
-void ioc_ok_ack( queue_t *, mblk_t *, int );
-void ioc_error_ack( queue_t *, mblk_t *, int );
-void ioc_copyin( queue_t *, mblk_t *, mblk_t *, caddr_t, uint );
-void ioc_copyout( queue_t *, mblk_t *, mblk_t *, caddr_t, caddr_t, uint );
