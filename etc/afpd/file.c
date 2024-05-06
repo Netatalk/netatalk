goshhhy/netatalk-classic

Explicitly initialize variable to quell warning on NetBSD.

etc/afpd/file.c
@@ -905,7 +905,8 @@ int setfilparams(struct vol *vol,
 	char *upath;
 	u_char achar, xyy[4];
 	u_char *fdType = NULL;	/* "uninitialized, OK 310105" -- yeah, no */
-	u_int16_t ashort, bshort, oshort;
+	u_int16_t ashort = 0;
+	u_int16_t bshort, oshort;
 	u_int32_t aint;
 	u_int32_t upriv;
 	u_int16_t upriv_bit = 0;

Netatalk/netatalk

etc/afpd/file.c
@@ -829,7 +829,8 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
     char		*ade = NULL;
     u_char              achar, xyy[4];
     const u_char        *fdType = NULL;
-    uint16_t		ashort, bshort, oshort;
+    uint16_t		ashort = 0;
+    uint16_t		bshort, oshort;
     uint32_t		aint;
     uint32_t		upriv;
     uint16_t           upriv_bit = 0;