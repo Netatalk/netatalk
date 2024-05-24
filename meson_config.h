33ab30b466259f2dbc59d59ff5abca14df4b6377
dgsga <181612+dgsga@users.noreply.github.com>
2024-05-24 19:33:04 +0100

    Revert "Update meson_config.h"
    This reverts commit 64750e2467c3d33d3db52e749d65047c6923721c.

--- meson_config.h
+++ meson_config.h
@@ -695,17 +695,14 @@
 
 /* WolfSSL configuration */
 //#include <wolfssl/options.h>
-#define HAVE_DH_DEFAULT_PARAMS 1
-#define HAVE_TLS_EXTENSIONS 1
+#define OPENSSL_EXTRA 1
+#define OPENSSL_ALL 1
+#define WOLFSSL_DES_ECB 1
+#define NO_SHA 1
+#define NO_OLD_TLS 1
 #define NO_MD4 1
 #define NO_MD5 1
-#define NO_OLD_TLS 1
-#define NO_SHA 1
-#define OPENSSL_ALL 1
-#define OPENSSL_EXTRA 1
-#define WOLFSSL_DES_ECB 1
-#define WOLFSSL_SSL_BN_INCLUDED 1
-#define WOLFSSL_SSL_CRYPTO_INCLUDED 1
+#define HAVE_TLS_EXTENSIONS 1
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
