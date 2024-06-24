8c891963c68d7c3dbf38deb8692c7b845ac22215
Daniel Markstedt <daniel@mindani.net>
2024-06-24 14:03:32 +0900

    Add copyright for mac_roman.h
    All code in this file is lifted verbatim from lib/mac_roman.h in libiconv.
    
    Therefore, adding the same copyright year and author as in libiconv 1.9.2 which was contemporaneous to when mac_roman.h was added to Netatalk.
    
    https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.9.2.tar.gz

--- libatalk/unicode/charsets/mac_roman.h
+++ libatalk/unicode/charsets/mac_roman.h
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 1999-2001 Free Software Foundation, Inc.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
