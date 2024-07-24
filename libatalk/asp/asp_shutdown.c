983e15c9adb5dfb1976f8c37498b2ed3871b3ec7
Daniel Markstedt <daniel@mindani.net>
2024-07-24 21:26:13 +0900

    Reintroduce AppleTalk modules in libatalk

---
+++ libatalk/asp/asp_shutdown.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 1996 Regents of The University of Michigan.
+ * All Rights Reserved.  See COPYRIGHT.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/uio.h>
+#include <netatalk/endian.h>
+#include <netatalk/at.h>
+#include <atalk/atp.h>
+#include <atalk/asp.h>
+
+int asp_shutdown(ASP asp)
+{
+    struct atp_block	atpb;
+    struct iovec	iov;
+    char		*p;
+    uint16_t		seq;
+    uint8_t		oport;
+
+
+    p = asp->commands;
+    *p++ = ASPFUNC_CLOSE;
+    *p++ = asp->asp_sid;
+    seq = 0;
+    memcpy( p, &seq, sizeof(seq));
+    p += sizeof(seq);
+
+    oport = asp->asp_sat.sat_port;
+    atpb.atp_saddr = &asp->asp_sat;
+    atpb.atp_saddr->sat_port = asp->asp_wss;
+    atpb.atp_sreqdata = asp->commands;
+    atpb.atp_sreqdlen = p - asp->commands;
+    atpb.atp_sreqto = 2;
+    atpb.atp_sreqtries = 5;
+
+    if ( atp_sreq( asp->asp_atp, &atpb, 1, ATP_XO ) < 0 ) {
+	asp->asp_sat.sat_port = oport;
+	return( -1 );
+    }
+
+    iov.iov_base = asp->commands;
+    iov.iov_len = ASP_CMDSIZ;
+    atpb.atp_rresiov = &iov;
+    atpb.atp_rresiovcnt = 1;
+
+    if ( atp_rresp( asp->asp_atp, &atpb ) < 0 ) {
+	asp->asp_sat.sat_port = oport;
+	return( -1 );
+    }
+    asp->asp_sat.sat_port = oport;
+
+    return( 0 );
+}
---
+++ libatalk/netddp/netddp_open.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 1999 Adrian Sun (asun@zoology.washington.edu)
+ * Copyright (c) 1990,1991 Regents of The University of Michigan.
+ * All Rights Reserved. See COPYRIGHT.
+ *
+ * open a ddp socket and return the port and address assigned. return
+ * various address info if requested as well.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netatalk/at.h>
+#include <atalk/netddp.h>
+
+int netddp_open(struct sockaddr_at *addr, struct sockaddr_at *bridge)
+{
+
+#ifdef NO_DDP
+    return -1;
+#else /* !NO_DDP */
+
+    int s;
+    socklen_t len;
+
+    if ((s = socket( AF_APPLETALK, SOCK_DGRAM, 0 )) < 0) 
+	return -1;
+    
+    if (!addr)
+	return s;
+
+    addr->sat_family = AF_APPLETALK;
+    /* rest of address should be initialized by the caller */
+    if (bind(s, (struct sockaddr *) addr, sizeof( struct sockaddr_at )) < 0 ) {
+        close(s);
+	return -1;
+    }
+
+    /* get the real address from the kernel */
+    len = sizeof( struct sockaddr_at);
+    if ( getsockname( s, (struct sockaddr *) addr, &len ) != 0 ) {
+        close(s);
+	return -1;
+    }
+
+    return s;
+#endif /* NO_DDP */
+}
