4b5440b25a47b97822b49cbc8ac99c8b4b17fe29
Daniel Markstedt <daniel@mindani.net>
2024-07-24 21:26:13 +0900

    Reintroduce AppleTalk modules in libatalk

---
+++ libatalk/netddp/netddp_recvfrom.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 1999 Adrian Sun (asun@zoology.washington.edu)
+ * Copyright (c) 1990,1991 Regents of The University of Michigan.
+ * All Rights Reserved. See COPYRIGHT.
+ *
+ * receive data.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#ifndef NO_DDP
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <errno.h>
+
+#include <netatalk/at.h>
+#include <netatalk/endian.h>
+#include <netatalk/ddp.h>
+#include <atalk/netddp.h>
+
+#ifndef MAX
+#define MAX(a, b)  ((a) < (b) ? (b) : (a))
+#endif /* ! MAX */
+#endif /* no ddp */
