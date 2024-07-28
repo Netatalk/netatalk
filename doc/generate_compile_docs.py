2d3b720cf70f585dcf05b2c289618710979e06a4
Daniel Markstedt <daniel@mindani.net>
2024-07-28 09:56:53 +0900

    Add copyright grant to the script header

--- doc/generate_compile_docs.py
+++ doc/generate_compile_docs.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 # This script generates the compile.xml manual page from the GitHub build.yml file.
+#
+# (c) 2024 Daniel Markstedt <daniel@mindani.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
 
 import datetime
 import re
