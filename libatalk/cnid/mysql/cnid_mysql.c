a8a7a8e2a1ccbe9170157a9872e49d912fb2f298
Daniel Markstedt <daniel@mindani.net>
2025-01-12 00:04:48 -0800

    Add GPL v2 license grant to mysql CNID backend code
    Ralph Boehme approved this change in GitHub #1874

--- libatalk/cnid/mysql/cnid_mysql.c
+++ libatalk/cnid/mysql/cnid_mysql.c
@@ -1,6 +1,11 @@
 /*
  * Copyright (C) Ralph Boehme 2013
  * All Rights Reserved.  See COPYING.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #ifdef HAVE_CONFIG_H
