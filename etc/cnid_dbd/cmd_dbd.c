d8498abb7411ea5bca1530c592af79eba03e33e4
Daniel Markstedt <daniel@mindani.net>
2025-01-22 19:27:03 +0100

    Introduce CNID backend built on SQLite, GitHub #1177
    Based on the MySQL CNID backend by Ralph Boehme.
    Initial proof-of-concept by Christopher Kobayashi
    
    Adds optional dependency on the sqlite3 shared library
    
    Signed-off-by: Daniel Markstedt <daniel@mindani.net>

--- etc/cnid_dbd/cmd_dbd.c
+++ etc/cnid_dbd/cmd_dbd.c
@@ -263,7 +263,9 @@ int main(int argc, char **argv)
 
     /* open volume */
     if (STRCMP(vol->v_cnidscheme, !=, "dbd")
-            && STRCMP(vol->v_cnidscheme, !=, "mysql")) {
+            && STRCMP(vol->v_cnidscheme, !=, "mysql")
+            && STRCMP(vol->v_cnidscheme, !=, "sqlite")
+       ) {
         dbd_log(LOGSTD, "\"%s\" isn't a \"dbd\" CNID volume", vol->v_path);
         exit(EXIT_FAILURE);
     }
