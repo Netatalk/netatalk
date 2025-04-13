56f95eac9499e0ba0a46456bdcae338f9dbea026
Daniel Markstedt <daniel@mindani.net>
2025-04-13 09:33:07 +0200

    testsuite: Break out separate FPGetExtAttr test module
    Create a new FPGetExtAttr test module and move three tests
    from other modules here
    
    Also improves several test skipped reason descriptions

--- test/testsuite/specs.h
+++ test/testsuite/specs.h
@@ -85,7 +85,9 @@ extern int not_valid_bitmap(unsigned int ret, unsigned int bitmap, int afpd_erro
 #define T_PATH       2
 #define T_AFP3       3
 #define T_AFP3_CONN2 4
-/* #define T_MAC_PATH   5 */
+#if 0
+#define T_MAC_PATH   5
+#endif
 #define T_UNIX_PREV  6
 #define T_UTF8       7
 #define T_VOL2       8
