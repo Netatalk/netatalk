f926a954dc20fc6da3319c97799199c542d3e411
Daniel Markstedt <daniel@mindani.net>
2025-04-20 12:40:57 +0200

    meson: Use pandoc to generate documentation when available
    When detected, use pandoc instead of cmark-gfm or cmark
    for generating plaintext, roff, and html documentation
    
    pandoc has reportedly the highest quality transcoding output,
    but relies on the heavy-weight Haskell stack, so cmark is still
    a much more light-weight option when the build system has
    resource constraints
    
    Additionally, when neither pandoc nor cmark-gfm are detected,
    install the raw Markdown readmes instead of nothing

--- doc/manpages/make_man.sh
+++ doc/manpages/make_man.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
 if [ "$#" -ne 5 ]; then
-  echo "Usage: $0 <cmark> <file> <manpage> <section> <version>"
+  echo "Usage: $0 <transcoder> <file> <manpage> <section> <version>"
   exit 1
 fi
 
-cmark="$1"
+transcoder="$1"
 input="$2"
 man="$3"
 sec="$4"
 ver="$5"
 
+transcoder_name=$(basename "${transcoder}")
+
 echo ".TH \"${man}\" \"${sec}\" \"\" \"Netatalk ${ver}\" \"Netatalk AFP Fileserver Manual\""
 
-${cmark} --smart --to man ${input}
+if [ "${transcoder_name}" = "pandoc" ]; then
+  ${transcoder} --from commonmark --to man ${input}
+elif [ "${transcoder_name}" = "cmark" ] || [ "${transcoder_name}" = "cmark-gfm" ]; then
+  ${transcoder} --smart --to man ${input}
+else
+  echo "Unknown transcoder: ${transcoder_name}"
+  exit 1
+fi
