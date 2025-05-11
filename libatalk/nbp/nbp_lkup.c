9f4afb0ef11f6d75244aa785ade71dff211fb1e3
cheesestraws <rob@midworld.co.uk>
2025-05-11 12:47:31 +0100

    nbp: normalise code style a bit in nbp_lkup.c

--- libatalk/nbp/nbp_lkup.c
+++ libatalk/nbp/nbp_lkup.c
@@ -32,27 +32,27 @@
 #define SOCKLEN_T unsigned int
 #endif /* ! SOCKLEN_T */
 
-int nbp_lookup( const char *obj, const char *type, const char *zone, struct nbpnve *nn,
-    int			nncnt,
-    const struct at_addr *ataddr)
-{
-    struct sockaddr_at	addr, from;
-    struct timeval	tv, tv_begin, tv_end;
-    fd_set		fds;
-    struct nbpnve	nve;
-    struct nbphdr	nh;
-    struct nbptuple	nt;
-    struct servent	*se;
-    char		*data = nbp_send;
-    SOCKLEN_T   	namelen;
-    int			s, cnt, tries, sc, cc, i, c;
+int nbp_lookup(const char *obj, const char *type, const char *zone, struct nbpnve *nn,
+                int nncnt, const struct at_addr *ataddr) {
+    struct sockaddr_at addr, from;
+    struct timeval tv, tv_begin, tv_end;
+    fd_set fds;
+    struct nbpnve nve;
+    struct nbphdr nh;
+    struct nbptuple nt;
+    struct servent *se;
+    char *data = nbp_send;
+    SOCKLEN_T namelen;
+    int s, cnt, tries, sc, cc, i, c;
 
     memset(&addr, 0, sizeof(addr));
     memset(&from, 0, sizeof(from));
-    if (ataddr)
-      memcpy(&addr.sat_addr, ataddr, sizeof(struct at_addr));
-    if ((s = netddp_open(&addr, &from)) < 0)
-      return -1;
+    if (ataddr) {
+        memcpy(&addr.sat_addr, ataddr, sizeof(struct at_addr));
+    }
+    if ((s = netddp_open(&addr, &from)) < 0) {
+        return -1;
+    }
 
     *data++ = DDPTYPE_NBP;
     nh.nh_op = NBPOP_BRRQ;
@@ -71,41 +71,47 @@ int nbp_lookup( const char *obj, const char *type, const char *zone, struct nbpn
     data += SZ_NBPTUPLE;
 
     if ( obj ) {
-	if (( cc = strlen( obj )) > NBPSTRLEN ) goto lookup_err;
-	*data++ = cc;
-	memcpy( data, obj, cc );
-	data += cc;
+        if (( cc = strlen( obj )) > NBPSTRLEN ) {
+            goto lookup_err;
+        }
+        *data++ = cc;
+        memcpy( data, obj, cc );
+        data += cc;
     } else {
-	*data++ = 1;
-	*data++ = '='; /* match anything */
+        *data++ = 1;
+        *data++ = '='; /* match anything */
     }
 
     if ( type ) {
-	if (( cc = strlen( type )) > NBPSTRLEN ) goto lookup_err;
-	*data++ = cc;
-	memcpy( data, type, cc );
-	data += cc;
+        if (( cc = strlen( type )) > NBPSTRLEN ) {
+            goto lookup_err;
+        }
+        *data++ = cc;
+        memcpy( data, type, cc );
+        data += cc;
     } else {
-	*data++ = 1;
-	*data++ = '='; /* match anything */
+        *data++ = 1;
+        *data++ = '='; /* match anything */
     }
 
     if ( zone ) {
-	if (( cc = strlen( zone )) > NBPSTRLEN ) goto lookup_err;
-	*data++ = cc;
-	memcpy( data, zone, cc );
-	data += cc;
+        if (( cc = strlen( zone )) > NBPSTRLEN ) {
+            goto lookup_err;
+        }
+        *data++ = cc;
+        memcpy( data, zone, cc );
+        data += cc;
     } else {
-	*data++ = 1;
-	*data++ = '*'; /* default zone */
+        *data++ = 1;
+        *data++ = '*'; /* default zone */
     }
 
     if ( nbp_port == 0 ) {
-      if (( se = getservbyname( "nbp", "ddp" )) == NULL ) {
-	            nbp_port = 2;
-      } else {
-	            nbp_port = ntohs( se->s_port );
-      }
+        if (( se = getservbyname( "nbp", "ddp" )) == NULL ) {
+            nbp_port = 2;
+        } else {
+            nbp_port = ntohs( se->s_port );
+        }
     }
 
     addr.sat_port = nbp_port;
@@ -114,87 +120,87 @@ int nbp_lookup( const char *obj, const char *type, const char *zone, struct nbpn
     tries = 3;
     sc = data - nbp_send;
     while ( tries > 0 ) {
-	if ( netddp_sendto( s, nbp_send, sc, 0, (struct sockaddr *)&addr,
-		sizeof( struct sockaddr_at )) < 0 ) {
-	    goto lookup_err;
-	}
+        if ( netddp_sendto( s, nbp_send, sc, 0, (struct sockaddr *)&addr,
+                            sizeof( struct sockaddr_at )) < 0 ) {
+            goto lookup_err;
+        }
 
-	tv.tv_sec = 2L;
-	tv.tv_usec = 0;
+        tv.tv_sec = 2L;
+        tv.tv_usec = 0;
 
-	for (;;) {
-	    FD_ZERO( &fds );
-	    FD_SET( s, &fds );
-	    if ( gettimeofday( &tv_begin, NULL ) < 0 ) {
-		goto lookup_err;
-	    }
-	    if (( c = select( s + 1, &fds, NULL, NULL, &tv )) < 0 ) {
-		goto lookup_err;
-	    }
-	    if ( c == 0 || FD_ISSET( s, &fds ) == 0 ) {
-		break;
-	    }
-	    if ( gettimeofday( &tv_end, NULL ) < 0 ) {
-		goto lookup_err;
-	    }
-	    if ( tv_begin.tv_usec > tv_end.tv_sec ) {
-		tv_end.tv_usec += 1000000;
-		tv_end.tv_sec -= 1;
-	    }
-	    if (( tv.tv_usec -= ( tv_end.tv_usec - tv_begin.tv_usec )) < 0 ) {
-		tv.tv_usec += 1000000;
-		tv.tv_sec -= 1;
-	    }
-	    if (( tv.tv_sec -= ( tv_end.tv_sec - tv_begin.tv_sec )) < 0 ) {
-		break;
-	    }
+        for (;;) {
+            FD_ZERO( &fds );
+            FD_SET( s, &fds );
+            if ( gettimeofday( &tv_begin, NULL ) < 0 ) {
+                goto lookup_err;
+            }
+            if (( c = select( s + 1, &fds, NULL, NULL, &tv )) < 0 ) {
+                goto lookup_err;
+            }
+            if ( c == 0 || FD_ISSET( s, &fds ) == 0 ) {
+                break;
+            }
+            if ( gettimeofday( &tv_end, NULL ) < 0 ) {
+                goto lookup_err;
+            }
+            if ( tv_begin.tv_usec > tv_end.tv_sec ) {
+                tv_end.tv_usec += 1000000;
+                tv_end.tv_sec -= 1;
+            }
+            if (( tv.tv_usec -= ( tv_end.tv_usec - tv_begin.tv_usec )) < 0 ) {
+                tv.tv_usec += 1000000;
+                tv.tv_sec -= 1;
+            }
+            if (( tv.tv_sec -= ( tv_end.tv_sec - tv_begin.tv_sec )) < 0 ) {
+                break;
+            }
 
-	    namelen = sizeof( struct sockaddr_at );
-	    if (( cc = netddp_recvfrom( s, nbp_recv, sizeof( nbp_recv ), 0,
-		    (struct sockaddr *)&from, &namelen )) < 0 ) {
-		goto lookup_err;
-	    }
+            namelen = sizeof( struct sockaddr_at );
+            if (( cc = netddp_recvfrom( s, nbp_recv, sizeof( nbp_recv ), 0,
+                                        (struct sockaddr *)&from, &namelen )) < 0 ) {
+                goto lookup_err;
+            }
 
-	    data = nbp_recv;
-	    if ( *data++ != DDPTYPE_NBP ) {
-		continue;
-	    }
-	    cc--;
+            data = nbp_recv;
+            if ( *data++ != DDPTYPE_NBP ) {
+                continue;
+            }
+            cc--;
 
-	    memcpy( &nh, data, SZ_NBPHDR );
-	    data += SZ_NBPHDR;
-	    if ( nh.nh_op != NBPOP_LKUPREPLY ) {
-		continue;
-	    }
-	    cc -= SZ_NBPHDR;
+            memcpy( &nh, data, SZ_NBPHDR );
+            data += SZ_NBPHDR;
+            if ( nh.nh_op != NBPOP_LKUPREPLY ) {
+                continue;
+            }
+            cc -= SZ_NBPHDR;
 
-	    while (( i = nbp_parse( data, &nve, cc )) >= 0 ) {
-		data += cc - i;
-		cc = i;
-		/*
-		 * Check to see if nve is already in nn. If not,
-		 * put it in, and increment cnt.
-		 */
-		for ( i = 0; i < cnt; i++ ) {
-		    if ( nbp_match( &nve, &nn[ i ],
-			    NBPMATCH_NOZONE|NBPMATCH_NOGLOB )) {
-			break;
-		    }
-		}
-		if ( i == cnt ) {
-		    nn[ cnt++ ] = nve;
-		}
-		if ( cnt == nncnt ) {
-		    tries = 0;
-		    break;
-		}
-	    }
-	    if ( cnt == nncnt ) {
-		tries = 0;
-		break;
-	    }
-	}
-	tries--;
+            while (( i = nbp_parse( data, &nve, cc )) >= 0 ) {
+                data += cc - i;
+                cc = i;
+                /*
+                 * Check to see if nve is already in nn. If not,
+                 * put it in, and increment cnt.
+                 */
+                for ( i = 0; i < cnt; i++ ) {
+                    if ( nbp_match( &nve, &nn[ i ],
+                                    NBPMATCH_NOZONE|NBPMATCH_NOGLOB )) {
+                        break;
+                    }
+                }
+                if ( i == cnt ) {
+                    nn[ cnt++ ] = nve;
+                }
+                if ( cnt == nncnt ) {
+                    tries = 0;
+                    break;
+                }
+            }
+            if ( cnt == nncnt ) {
+                tries = 0;
+                break;
+            }
+        }
+        tries--;
     }
 
     netddp_close(s);
